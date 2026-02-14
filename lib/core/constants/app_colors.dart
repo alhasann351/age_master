1458639b013664fd648ba6548317318c1eecb3d6
Al-Hasan <alhasann352@gmail.com>
2026-02-14 23:38:50 +0600

    some colors add

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color lightPrimaryColor = Color(0xFF4F46E5);
+  static const Color darkPrimaryColor = Color(0xFF818CF8);
+  static const Color lightSecondaryColor = Color(0xFF7C3AED);
+  static const Color darkSecondaryColor = Color(0xFFA78BFA);
+  static const Color lightAccentHighlightColor = Color(0xFF06B6D4);
+  static const Color darkAccentHighlightColor = Color(0xFF22D3EE);
+  static const Color lightScaffoldBackgroundColor = Color(0xFFF8FAFC);
+  static const Color darkScaffoldBackgroundColor = Color(0xFF0F172A);
+  static const Color lightCardColor = Color(0xFFFFFFFF);
+  static const Color darkCardColor = Color(0xFF1E293B);
+  static const Color cardBorderColor = Color(0xFFE2E8F0);
+  static const Color lightPrimaryTextColor = Color(0xFF0F172A);
+  static const Color darkPrimaryTextColor = Color(0xFFF1F5F9);
+  static const Color lightSecondaryTextColor = Color(0xFF475569);
+  static const Color darkSecondaryTextColor = Color(0xFF94A3B8);
+  static const Color specialHighlightColor = Color(0xFF22C55E);
+  static const Color warningColor = Color(0xFFEF4444);
+}
